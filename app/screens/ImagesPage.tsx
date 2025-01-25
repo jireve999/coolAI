ee211ff4fe767c255d06e07e0430f770b261b918
jireve999 <wbajxw999@aliyun.com>
2025-01-25 17:09:33 +0800

    Initial commit

---
+++ app/screens/ImagesPage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ImagesPage = () => {
+  return (
+    <View>
+      <Text>ImagesPage</Text>
+    </View>
+  );
+};
+
+export default ImagesPage;
---
+++ app/screens/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const SeetingsPage = () => {
+  return (
+    <View>
+      <Text>SeetingsPage</Text>
+    </View>
+  );
+};
+
+export default SeetingsPage;
